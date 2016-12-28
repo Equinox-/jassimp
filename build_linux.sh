070bca8d06296ccf2bf1f2245dac5f8bc25d8dac
Westin Miller <equinoxscripts@gmail.com>
2016-12-27 16:06:47 -0800

    Maven compatible

--- build_linux.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/zsh
-cd jassimp-native/src
-BASE=../bin/linux/x64/
-LIBNAME=libjassmip.so
-CC=gcc
-$CC jassimp.cpp -I/usr/lib/jvm/default/include/ -I/usr/lib/jvm/default/include/linux -I/usr/include/assimp/ -L$BASE -lassimp -shared -fPIC -o $BASE$LIBNAME
